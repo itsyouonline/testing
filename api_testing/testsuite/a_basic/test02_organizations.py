f9a3e766b018e2adb7a2bf412edf377516c4b4fa
Mahmoud Ali <simplymahmoud@gmail.com>
2016-11-16 12:36:44 +0200

    last push

--- api_testing/testsuite/a_basic/test02_organizations.py
+++ api_testing/testsuite/a_basic/test02_organizations.py
@@ -1,6 +1,6 @@
 from testing.api_testing.utils import BaseTest
 import types
-import unittest
+import uuid
 
 
 class OrganizationsTests(BaseTest):
@@ -11,13 +11,11 @@ class OrganizationsTests(BaseTest):
         response = self.client.api.GetUserOrganizations(self.user)
         self.lg('GetUserOrganizations [%s] response [%s]' % (self.user, response.json()))
         self.assertEqual(response.status_code, 200)
-        self.organization_id = response.json()['owner'][0]
-        self.lg('organization_id %s' % self.organization_id)
+        self.globalid = response.json()['owner'][0]
+        self.lg('organization globalid %s' % self.globalid)
 
-    #Currently fail due to issue https://github.com/itsyouonline/identityserver/issues/233
-    #@unittest.skip("fail due to issue https://github.com/itsyouonline/identityserver/issues/233")
-    def test001_get_organization(self):
-        """ ITSYOU-013
+    def test001_post_put_delete_organization(self):
+        """ ITSYOU-
         *Test case for check get organization GET /organizations/{globalid}.*
 
         **Test Scenario:**
@@ -26,78 +24,132 @@ class OrganizationsTests(BaseTest):
         #. validate all expected keys in the returned response
         """
         self.lg('%s STARTED' % self._testID)
+        username = self.user
+        globalid = str(uuid.uuid4()).replace('-', '')[0:10]
+        data = {'globalid': globalid,
+                'dns': [globalid + '.com'],
+                'includes': '',
+                'owners': [username],
+                'members': [],
+                'publicKeys': []}
+        self.client.api.CreateNewOrganization(data)
+        response = self.client.api.GetUserOrganizations(self.user)
+        self.lg('GetUserOrganizations [%s] response [%s]' % (username, response.json()))
+        self.assertEqual(response.status_code, 200)
+        self.client.api.AddOrganizationMember(data, globalid)
+        self.client.api.AddOrganizationOwner(data, globalid)
+        self.client.api.RemovePendingOrganizationInvitation(username, globalid)
+        self.client.api.LeaveOrganization(globalid, username)
+        self.client.api.UpdateOrganization(data, globalid)
+        self.client.api.DeleteOrganization(globalid)
         response = self.client.api.GetOrganization(self.organization_id)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.json()), types.DictType)
         self.lg('%s ENDED' % self._testID)
 
-    def test002_get_organization_tree(self):
-        """ ITSYOU-014
-        *Test case for check get organization tree GET /organizations/{globalid}/tree.*
+    def test002_post_put_delete_organization(self):
+        """ ITSYOU-
+        *Test case for check get organization GET /organizations/{globalid}.*
 
         **Test Scenario:**
 
-        #. check get organizations tree, should succeed
+        #. check get organizations, should succeed
         #. validate all expected keys in the returned response
         """
         self.lg('%s STARTED' % self._testID)
-        response = self.client.api.GetOrganizationTree(self.organization_id)
+        username = self.user
+        globalid = str(uuid.uuid4()).replace('-', '')[0:10]
+        data = {'globalid': globalid,
+                'dns': [globalid + '.com'],
+                'includes': '',
+                'owners': [self.user],
+                'members': [],
+                'publicKeys': []}
+        self.client.api.CreateOrganizationDNS(data, dnsname, globalid)
+        self.client.api.UpdateOrganizationDNS(data, dnsname, globalid)
+        self.client.api.DeleteOrganizaitonDNS(dnsname, globalid)
+        response = self.client.api.GetOrganization(self.organization_id)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.json()), types.DictType)
         self.lg('%s ENDED' % self._testID)
 
-    #Currently fail due to issue https://github.com/itsyouonline/identityserver/issues/233
-    #@unittest.skip("fail due to issue https://github.com/itsyouonline/identityserver/issues/233")
-    def test003_get_organization_contracts(self):
-        """ ITSYOU-015
-        *Test case for check get organization contracts GET /organizations/{globalid}/contracts.*
+    def test003_post_put_delete_organization(self):
+        """ ITSYOU-
+        *Test case for check get organization GET /organizations/{globalid}.*
 
         **Test Scenario:**
 
-        #. check get organizations contracts, should succeed
+        #. check get organizations, should succeed
         #. validate all expected keys in the returned response
         """
         self.lg('%s STARTED' % self._testID)
-        response = self.client.api.GetOrganizationContracts(self.organization_id)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(type(response.json()), types.ListType)
-        self.lg('%s ENDED' % self._testID)
-
-    #Currently fail due to issue https://github.com/itsyouonline/identityserver/issues/233
-    #@unittest.skip("fail due to issue https://github.com/itsyouonline/identityserver/issues/233")
-    def test004_get_organization_invitations(self):
-        """ ITSYOU-016
-        *Test case for check get organization invitations GET /organizations/{globalid}/invitations.*
-
-        **Test Scenario:**
-
-        #. check get organizations invitations, should succeed
-        #. validate all expected keys in the returned response
-        """
-        self.lg('%s STARTED' % self._testID)
-        response = self.client.api.GetPendingOrganizationInvitations(self.organization_id)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(type(response.json()), types.ListType)
-        self.lg('%s ENDED' % self._testID)
-
-    #Currently fail due to issue https://github.com/itsyouonline/identityserver/issues/233
-    #@unittest.skip("fail due to issue https://github.com/itsyouonline/identityserver/issues/233")
-    def test005_get_organization_apikeys(self):
-        """ ITSYOU-017
-        *Test case for check get organization apikeys GET /organizations/{globalid}/apikeys.*
-
-        **Test Scenario:**
-
-        #. check get organizations apikeys, should succeed
-        #. validate all expected keys in the returned response
-        """
-        self.lg('%s STARTED' % self._testID)
-        response = self.client.api.GetOrganizationAPIKeyLabels(self.organization_id)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(type(response.json()), types.ListType)
-        label = response.json()[0]
-        response = self.client.api.GetOrganizationAPIKey(label, self.organization_id)
+        username = self.user
+        globalid = str(uuid.uuid4()).replace('-', '')[0:10]
+        data = {'globalid': globalid,
+                'dns': [globalid + '.com'],
+                'includes': '',
+                'owners': [self.user],
+                'members': [],
+                'publicKeys': []}
+        self.client.api.CreateNewOrganizationAPIKey(data, globalid)
+        self.client.api.GetOrganizationAPIKey(data, globalid)
+        self.client.api.GetOrganizationAPIKeyLabels(globalid)
+        self.client.api.UpdateOrganizationAPIKey(data, label, globalid)
+        self.client.api.DeleteOrganizationAPIKey(label, globalid)
+        response = self.client.api.GetOrganization(self.organization_id)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.json()), types.DictType)
         self.lg('%s ENDED' % self._testID)
 
+    def test004_post_put_delete_organization(self):
+        """ ITSYOU-
+        *Test case for check get organization GET /organizations/{globalid}.*
+
+        **Test Scenario:**
+
+        #. check get organizations, should succeed
+        #. validate all expected keys in the returned response
+        """
+        self.lg('%s STARTED' % self._testID)
+        username = self.user
+        globalid = str(uuid.uuid4()).replace('-', '')[0:10]
+        data = {'globalid': globalid,
+                'dns': [globalid + '.com'],
+                'includes': '',
+                'owners': [self.user],
+                'members': [],
+                'publicKeys': []}
+        self.client.api.CreateNewSubOrganization(data, globalid)
+        self.client.api.AddOrganizationRegistryEntry(data, globalid)
+        self.client.api.AddUserRegistryEntry(data, globalid)
+        self.client.api.UpdateOrganizationMemberShip(data, globalid)
+        self.client.api.DeleteOrganizationRegistryEntry(key, globalid)
+        response = self.client.api.GetOrganization(self.organization_id)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(type(response.json()), types.DictType)
+        self.lg('%s ENDED' % self._testID)
+
+    def test005_post_put_delete_organization(self):
+        """ ITSYOU-
+        *Test case for check get organization GET /organizations/{globalid}.*
+
+        **Test Scenario:**
+
+        #. check get organizations, should succeed
+        #. validate all expected keys in the returned response
+        """
+        self.lg('%s STARTED' % self._testID)
+        username = self.user
+        globalid = str(uuid.uuid4()).replace('-', '')[0:10]
+        data = {'globalid': globalid,
+                'dns': [globalid + '.com'],
+                'includes': '',
+                'owners': [self.user],
+                'members': [],
+                'publicKeys': []}
+        self.client.api.CreateOrganizationContracty(data, globalid)
+        self.client.api.GetOrganizationContracts(globalid)
+        response = self.client.api.GetOrganization(self.organization_id)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(type(response.json()), types.DictType)
+        self.lg('%s ENDED' % self._testID)
